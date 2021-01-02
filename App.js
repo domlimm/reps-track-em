27de57957740f094db501678e80fb6625fee0536
domsterthebot <dominiclimkj3448@gmail.com>
2021-01-02 12:18:21 +0800

    Load fonts async.

--- App.js
+++ App.js
@@ -1,22 +1,36 @@
-import React from 'react';
-import { StyleSheet } from 'react-native';
+import React, { useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import * as Font from 'expo-font';
+import AppLoading from 'expo-app-loading';
 
 import AppNavigator from './src/navigation/AppNavigator';
 
+const fetchFonts = () => {
+  return Font.loadAsync({
+    'roboto-medium': require('./assets/fonts/Roboto-Medium.ttf')
+  });
+};
+
 export default function App() {
+  const [assetsLoaded, setAssetsLoaded] = useState(false);
+
+  const loadAssetsAsync = async () => {
+    await fetchFonts();
+  };
+
+  if (!assetsLoaded) {
+    return (
+      <AppLoading
+        startAsync={loadAssetsAsync}
+        onFinish={() => setAssetsLoaded(true)}
+        onError={console.warn}
+      />
+    );
+  }
+
   return (
     <NavigationContainer>
       <AppNavigator />
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
-});

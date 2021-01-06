5c12ac97bd0b190495bd9377b6bac6d29befefc0
domsterthebot <dominiclimkj3448@gmail.com>
2021-01-06 21:27:13 +0800

    added icons for card, to also randomly generate icon

--- src/constants/Variables.js
+++ src/constants/Variables.js
@@ -34,3 +34,20 @@ export const greeting = () => {
 
   return greeting;
 };
+
+import React from 'react';
+import { FontAwesome5, MaterialCommunityIcons } from '@expo/vector-icons';
+
+const CARD_ICONS = [
+  <MaterialCommunityIcons name='human' size={24} color='black' />,
+  <MaterialCommunityIcons name='human-handsup' size={24} color='black' />,
+  <FontAwesome5 name='weight' size={24} color='black' />,
+  <FontAwesome5 name='weight-hanging' size={24} color='black' />,
+  <MaterialCommunityIcons name='weight' size={24} color='black' />,
+  <MaterialCommunityIcons name='weight-kilogram' size={24} color='black' />,
+  <MaterialCommunityIcons name='weight-lifter' size={24} color='black' />,
+  <MaterialCommunityIcons name='weight-pound' size={24} color='black' />
+];
+
+export const generateCardIcon = () =>
+  CARD_ICONS[(CARD_ICONS.length * Math.random()) | 0];
